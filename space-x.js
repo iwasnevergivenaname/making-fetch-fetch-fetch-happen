41e544d1869be2ef56183ea9b0d150dc3764d890
iwasnevergivenaname <hannah.mace93@gmail.com>
2020-07-22 14:59:23 -0700

    [space-x.js] add space-x data to own file

---
+++ space-x.js
@@ -0,0 +1,37 @@
+let containerOne = document.querySelector(".container-one");
+
+// call
+fetch("https://api.spacexdata.com/v3/capsules")
+    //   ^ returns response v
+    .then(response => {
+        // data returned
+        return response.json();
+    })
+
+    //    with that ^data^
+    .then(data => {
+        const firstElement = data[0];
+        console.log(firstElement);
+        let capsuleID = firstElement.capsule_id;
+        console.log(capsuleID);
+//     let details = firstElement.details;
+//     console.log(details);
+//     let landings = firstElement.landings;
+//     console.log(landings);
+
+        for (let i = 0; i < data.length; i++) {
+            let eachCaspule = data[i];
+            const obj = {
+                capsuleID: eachCaspule.capsule_id,
+                details: eachCaspule.details,
+                landings: capsuleID.landings
+            };
+
+            console.log(obj);
+            const capsuleElement = document.createElement("p");
+            capsuleElement.textContent = obj.capsuleID;
+            containerOne.appendChild(capsuleElement);
+        }
+    });
+
+
